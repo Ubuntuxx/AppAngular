f86a84486b0f060632f92be08be6349723b33ede
jlagp <jlagp@customer.qrtomex1.pop.starlinkisp.net>
2023-06-18 21:45:58 -0600

    Creacion de cliente service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ClientesComponent } from './clientes/clientes.component'; 
+import { ClienteService } from './clientes/cliente.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { ClientesComponent } from './clientes/clientes.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
