b4eadc8af76951b83657c3588c6503b5a854a106
JLAGSuse <jlgap@duck.com>
2023-07-07 01:02:15 -0600

    Formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
-import {ClientesComponent} from './clientes/clientes.component';
-import {ClienteService} from './clientes/cliente.service';
-import {Routes, RouterModule} from '@angular/router';
-import {DirectivasComponent} from './directivas/directivas.component';
-import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
+import { Routes, RouterModule } from '@angular/router';
+import { DirectivasComponent } from './directivas/directivas.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormComponent } from './clientes/form.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
-  {path: 'directivas', component: DirectivasComponent},
-  {path: 'clientes', component: ClientesComponent}
+  { path: '', redirectTo: '/clientes', pathMatch: 'full' },
+  { path: 'directivas', component: DirectivasComponent },
+  { path: 'clientes', component: ClientesComponent }
 
 ]
 
@@ -23,11 +25,13 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     ClientesComponent,
-    DirectivasComponent
+    DirectivasComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-     HttpClientModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
